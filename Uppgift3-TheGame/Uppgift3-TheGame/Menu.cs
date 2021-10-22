4bee7c895d8be2595b0c7982ad16251e1778cb31
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-10-22 10:40:58 +0200

    First Commit

---
+++ Uppgift3-TheGame/Uppgift3-TheGame/Menu.cs
@@ -0,0 +1,23 @@
+﻿namespace MainMenu.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    
+
+    public class Menu
+    {
+        public List<string> MenuItems { get; set; } = new();
+        public int HeaderLines { get; set; } = 0;
+        public int InfoLines { get; set; } = 0;
+        public int MenuHeight { get; private set; } = 0;
+        public int MenuWidth { get; private set; } = 0;
+        public int HightLightRow { get; private set; } = 0;
+        public int StartX { get; private set; } = 0;
+        public int StartY { get; private set; } = 0;
+        public string topLine { get; private set; } = "";
+        public string midLine { get; private set; } = "";
+        public string bottomLine { get; private set; } = "";
+        public string HelpText { get; private set; } = " Arrowkeys to navigate, Enter to select.";
+        public int CurrentY { get; private set; }
+    }
+}
---
+++ Uppgift3-TheGame/Uppgift3-TheGame/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Uppgift3_TheGame
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
